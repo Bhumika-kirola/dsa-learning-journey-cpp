5af4ed54e8353597bd2649576db5ec7c42197128
Bhumika-Kirola <kirolabhumika9@gmail.com>
2026-09-12 23:47:13 +0530

    second largest num and removing the duplicate num

--- 8. Arrays/second_largest.cpp
+++ 8. Arrays/second_largest.cpp
@@ -12,6 +12,19 @@ void m(vector<int>& v, int n)
     sort(v.begin(), v.end());
     cout << v[n-1];
 }
+// optimized solution
+
+void m2 (vector <int>& v, int n)
+{
+    int largest = v[0];
+    for(int i = 0; i < n; i++)
+    {
+        if (largest <= v[i]) largest = v[i];
+    } 
+    cout << largest;
+}
+
+// the soting and all stuff not that necessary until specified 
 void method1(vector<int>& v, int n)
 {
     int min;
@@ -27,6 +40,91 @@ void method1(vector<int>& v, int n)
     cout << v[n-1];
 }
 
+
+// QUESTION 2 : YOU HAVE TO FIND THE SECOND LARGEST NUMBER IN THE GIVEN ARRAY OF NUMBERS
+
+// BRUTE METHOD
+void S_m0(vector<int>& v, int n)
+{
+    sort(v.begin(), v.end());
+    for (int i = n-2; i >= 0; i--)
+    {
+        if (v[i] != v[n-1])
+        {
+            cout << v[i];
+            break;
+        }
+    }
+}
+
+// better way
+void S_m(vector<int>& v, int n)
+{
+    int largest = v[0], s_largest = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if (largest <= v[i]) largest = v[i];
+    }
+    for (int j = 0; j < n; j ++)
+    {
+        if (v[j] >= s_largest && v[j] != largest) s_largest = v[j];
+    }
+    cout << s_largest;
+}
+
+// optimal solution 
+
+void S_M_O(vector<int>& v, int n)
+{
+    int largest = v[0], s_largest = - 1; //assuming the array contains only whole nums if there are negetave num use int min instead
+    for (int i = 1; i < n; i++)
+    {
+        if (largest < v[i])
+        {
+            s_largest = largest;
+            largest = v[i];
+        }
+    }
+    cout << largest << " " << s_largest;
+}
+
+
+// QUESTION 3: YOU HAVE BEEN GIVEN A ARRAY WHICH COINTAIN DUPLICATE VALUES YOU HAVVE TO MAKE THIS ARRAY A UNIQUE ARRAY
+
+
+// BRUTE METHOD : USE THE SET 
+
+void q_3_b(vector<int>& v, int n)
+{
+    set <int> s;
+    for (int i = 0 ; i < n; i++ )
+    {
+        s.insert(v[i]);
+    }
+    int index = 0;
+    for (auto it : s)
+    {
+        v[index] = it;
+        index++;
+    }
+    for (int i = 0; i < index ; i++) cout << v[i] << " ";
+}
+
+void q_3_o(vector<int>& v, int n)
+{
+    sort(v.begin(), v.end());
+    int j = 0;
+    for (int i = 1; i < n; i ++) 
+    {
+        if (v[j] != v[i])
+        {
+            j++;
+            v[j] = v[i];
+        }
+    }
+    for (int i = 0; i <= j; i++) cout << v[i] << " ";
+}
+
 int main()
 {
     vector<int> vec;
@@ -39,5 +137,5 @@ int main()
         cin >> element;
         vec.push_back(element);
     }
-    m(vec, n);
+    q_3_o(vec, n);
 }
